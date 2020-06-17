33557c05e692533461225916cfe7d5b1c8ea9788
ElbekDeveloper <e.normurodov@mail.ru>
2020-06-17 11:33:46 +0500

    feat: add classes

---
+++ TrackerLibrary/MatchupModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class MatchupModel
+    {
+         
+        public List<MatchupEntriesModel> Entries { get; set; }
+        /// <summary>
+        /// Represents winner team of the tournament
+        /// </summary>
+        public TeamModel Winner { get; set; }
+        /// <summary>
+        /// Represents the round of the game
+        /// </summary>
+        public int MatchupRound { get; set; }
+
+    }
+}
---
+++ TrackerLibrary/PrizeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class PrizeModel
+    {
+        public string PlaceNumber { get; set; }
+        public string PlaceName { get; set; }
+        public decimal PrizeAmout { get; set; }
+        public double PrizePercentage { get; set; }
+    }
+}
